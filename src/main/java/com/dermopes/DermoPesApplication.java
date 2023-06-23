b4e1a3dabfa6db991a33d14b033f40b79cd6b33b
Jakub Szymański <jakubszymanski@hotmail.com>
2023-06-23 23:14:45 +0200

    Initial commit

---
+++ src/main/java/com/dermopes/DermoPesApplication.java
@@ -0,0 +1,13 @@
+package com.dermopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DermoPesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DermoPesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dermopes/DermoPesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dermopes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DermoPesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
