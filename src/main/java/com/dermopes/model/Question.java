4324e78a9f40c606dabefca8a77c887cb209e477
Jakub Szymański <jakubszymanski@hotmail.com>
2023-06-23 23:48:40 +0200

    feature: added enum Category to question

--- src/main/java/com/dermopes/model/Question.java
+++ src/main/java/com/dermopes/model/Question.java
@@ -1,10 +1,12 @@
 package com.dermopes.model;
 
+import com.dermopes.model.enumeration.Category;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
@@ -18,4 +20,9 @@ public class Question {
     private String content;
     @OneToMany (mappedBy = "question", cascade = CascadeType.ALL)
     private List<Answer> answers;
+    private LocalDate examDate;
+    @ElementCollection(targetClass = Category.class)
+    @CollectionTable
+    @Enumerated(EnumType.STRING)
+    private List<Category> category;
 }
---
+++ src/main/java/com/dermopes/model/enumeration/Category.java
@@ -0,0 +1,8 @@
+package com.dermopes.model.enumeration;
+
+public enum Category {
+    TRICHISCOPY,
+    DERMOSCOPY,
+    BIOLOGICAL_TREATMENT,
+    NAIL_DISEASES
+}
